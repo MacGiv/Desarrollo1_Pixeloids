816f675504329a0a64d5ab151f80131a685a072f
MacGiv <55637723+MacGiv@users.noreply.github.com>
2024-10-29 20:29:19 -0300

    Add button.cpp

--- Desarrollo1_Pixeloids/button.cpp
+++ Desarrollo1_Pixeloids/button.cpp
@@ -1,1 +1,27 @@
 #include "button.h"
+#include "button.h"
+
+namespace pixeloids_luchelli 
+{
+
+    Button CreateButton(Vector2 position, Vector2 size, const char* text) 
+    {
+        Button button;
+        button.rect = { position.x, position.y, size.x, size.y };
+        button.text = text;
+        button.color = LIGHTGRAY;
+        return button;
+    }
+
+    void DrawButton(const Button& button) 
+    {
+        DrawRectangleRec(button.rect, button.color);
+        DrawText(button.text, button.rect.x + 10, button.rect.y + 10, 20, BLACK);
+    }
+
+    bool IsButtonClicked(const Button& button) 
+    {
+        return CheckCollisionPointRec(GetMousePosition(), button.rect) && IsMouseButtonPressed(MOUSE_BUTTON_LEFT);
+    }
+
+}
--- Desarrollo1_Pixeloids/button.h
+++ Desarrollo1_Pixeloids/button.h
@@ -2,21 +2,21 @@
 #define BUTTON_H
 
 #include "raylib.h"
-#include <conio.h>
 
 
-namespace pixeloids_luchelli {
+namespace pixeloids_luchelli 
+{
 
-    struct Button
-    {
-        Rectangle rect;
-        String text;
-        Color color;
-    };
+struct Button
+{
+    Rectangle rect;
+    const char* text;
+    Color color;
+};
 
-    Button CreateButton(Vector2 position, Vector2 size, String text);
-    void DrawButton(const Button& button);
-    bool IsButtonClicked(const Button& button);
+Button CreateButton(Vector2 position, Vector2 size, const char* text);
+void DrawButton(const Button& button);
+bool IsButtonClicked(const Button& button);
 
 }
 
