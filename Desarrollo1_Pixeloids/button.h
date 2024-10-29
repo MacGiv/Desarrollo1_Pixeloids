49f5c58f1e1442de0f8b82a92638cb39653ae716
MacGiv <tomasluchelli.17@gmail.com>
2024-10-29 20:11:22 -0300

    Add button.h

--- Desarrollo1_Pixeloids/button.h
+++ Desarrollo1_Pixeloids/button.h
@@ -1,2 +1,24 @@
+#ifndef BUTTON_H
+#define BUTTON_H
 
+#include "raylib.h"
+#include <conio.h>
+
+
+namespace pixeloids_luchelli {
+
+    struct Button
+    {
+        Rectangle rect;
+        String text;
+        Color color;
+    };
+
+    Button CreateButton(Vector2 position, Vector2 size, String text);
+    void DrawButton(const Button& button);
+    bool IsButtonClicked(const Button& button);
+
+}
+
+#endif 
 
